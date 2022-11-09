a6c5b15b78fc68a9f6cee8458d7821da458cb5c2
jacquelin-jimenez <jacquelinpeace@gmail.com>
2022-11-09 16:47:51 -0500

    fun file

---
+++ madlib.py
@@ -0,0 +1,10 @@
+from tkinter import N
+
+
+adj1 = input('Type an adjective: ')
+noun1= input('Type a plural noun: ')
+noun2 = input('Type a singlar noun: ')
+adj2 = input('Type another adjective: ')
+
+
+print('\n Grand Valley is {0}\n {1} are great\n {2} is hard\n but WIC is {3}'.format(adj1, noun1, noun2, adj2))
